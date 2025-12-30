6f7eebe8044d0bbd8d9271cc3df5535777972faf
Sanchez19sa <ssancheza@correo.iue.edu.co>
2025-12-30 00:46:51 -0500

    fix: index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,14 @@
-export * from './typing.types';
-export * from './results.types';
+export type {
+  TestMode,
+  Difficulty,
+  TestStatus,
+  SupportedLanguage,
+  CodeLanguage,
+  TestConfig,
+  CharState,
+  ChartPoint,
+  TestStats
+} from './typing.types';
+
+export type { ResultSummary } from './results.types';
 
